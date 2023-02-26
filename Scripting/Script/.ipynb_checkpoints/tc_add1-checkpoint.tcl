65d85864cb682a08f395e65af8813cdf946aa75d
Daniel Giftson E <daniel.giftson@iitgn.ac.in>
2023-02-27 00:02:57 +0530

    .ipynb_checkpoints deleted

--- Scripting/Script/.ipynb_checkpoints/tc_add1-checkpoint.tcl
+++
@@ -1,1 +0,0 @@
-create_project -force Test [pwd]/Automation -part xc7k325tffg900-2
--- Scripting/Script/.ipynb_checkpoints/tcl_v_add-checkpoint.tcl
+++
@@ -1,7 +0,0 @@
-create_project -force Automation [pwd]/Test -part xc7k325tffg900-2
-set_property board_part xilinx.com:kc705:part0:1.5 [current_project]
-update_compile_order -fileset sources_1
-
-update_compile_order -fileset sources_1
-
-close_project
